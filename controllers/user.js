87f93e367ded384c265be77088d896cd10983e39
Rina-ui <marinagracia895@gmail.com>
2025-07-16 13:45:41 +0200

    mise en palce du ligin

--- controllers/user.js
+++ controllers/user.js
@@ -22,7 +22,7 @@ export const registerUser = async (req, res) => {
 
         await user.save();
         res.status(201).json({message: 'User registered successfully'});
-        
+
     }catch(error) {
         console.error('Error registering user:', error);
         return res.status(500).json({message: 'Internal server error'});
@@ -30,5 +30,33 @@ export const registerUser = async (req, res) => {
 }
 
 export const loginUser = async (req, res) => {
+    const {username, password} = req.body;
 
+    try{
+        const user = await User.findOne({username: req.body.username});
+        if (!user) {
+            return res.status(404).json({error: 'User not found'});
+        }
+
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            return res.status(401).json({error: 'Invalid password'});
+        }
+
+        const token = jwt.sign(
+            {userId: user._id},
+            'RANDOM_TOKEN_SECRET',
+            {expiresIn: '24h'}
+            
+        )
+
+        return res.status(200).json({
+            userId: user._id,
+            token: token,
+        });
+
+    }catch (error) {
+        console.error('Error logging in user:', error);
+        return res.status(500).json({message: 'Internal server error'});
+    }
 }
