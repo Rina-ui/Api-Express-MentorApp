b13f446b0d994279b8efc1b54a30e14e88316300
Rina-ui <marinagracia895@gmail.com>
2025-07-21 13:16:18 +0200

    getUsdr controllers et routes

--- controllers/user.js
+++ controllers/user.js
@@ -61,3 +61,13 @@ export const loginUser = async (req, res) => {
         return res.status(500).json({message: 'Internal server error'});
     }
 }
+
+export const getAllUser = async (req, res) => {
+    try {
+        const users = await User.find().select('-password');
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        return res.status(500).json({message: 'Internal server error'});
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/user.js';
+import { getAllUser } from '../controllers/user.js'; 
 
 const router = express.Router();
 
 router.post('/register', registerUser);
-router.post('/loginUser', loginUser); // loginUser doit être une fonction ici
+router.post('/loginUser', loginUser); 
+router.get('/getAllUser', getAllUser); 
 
 export default router;
